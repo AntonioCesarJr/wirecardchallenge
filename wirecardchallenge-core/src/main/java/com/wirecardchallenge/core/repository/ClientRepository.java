07aaebef83ad390339047244d352db9b9a0ada38
AntonioCesarJr <jrcesar4@gmail.com>
2019-06-01 13:12:34 -0300

    Refactoring cache

--- wirecardchallenge-core/src/main/java/com/wirecardchallenge/core/repository/ClientRepository.java
+++ wirecardchallenge-core/src/main/java/com/wirecardchallenge/core/repository/ClientRepository.java
@@ -3,6 +3,7 @@ package com.wirecardchallenge.core.repository;
 import com.wirecardchallenge.core.entity.Client;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,10 +18,20 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 
     @Cacheable("clientsPage")
     Page<Client> findAll(Pageable pageable);
+    @Cacheable("clientId")
     Optional<Client> findById(Long id);
+    @Cacheable("clientUUID")
     Optional<Client> findByPublicId(UUID uuid);
-    @CacheEvict(value="clientsPage", allEntries=true)
+    @Caching(evict = {
+            @CacheEvict(value="clientsPage", allEntries=true),
+            @CacheEvict(value="clientsId", key = "#client.id"),
+            @CacheEvict(value="clientsUUID", key = "#client.publicId")
+    })
     Client save(Client client);
-    @CacheEvict(value="clientsPage", allEntries=true)
+    @Caching(evict = {
+            @CacheEvict(value="clientsPage", allEntries=true),
+            @CacheEvict(value="clientsId", key = "#client.id"),
+            @CacheEvict(value="clientsUUID", key = "#client.publicId")
+    })
     void delete(Client client);
 }
