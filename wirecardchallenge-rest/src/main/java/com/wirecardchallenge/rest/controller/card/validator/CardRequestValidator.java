41cd68ec37bd182d55db4623c3c0c84c576c1e68
AntonioCesarJr <jrcesar4@gmail.com>
2019-06-06 19:08:49 -0300

    Validating credit card number

--- wirecardchallenge-rest/src/main/java/com/wirecardchallenge/rest/controller/card/validator/CardRequestValidator.java
+++ wirecardchallenge-rest/src/main/java/com/wirecardchallenge/rest/controller/card/validator/CardRequestValidator.java
@@ -23,6 +23,7 @@ public class CardRequestValidator implements Validator {
         log.info("Card Validator in action !!");
         CardRequest cardRequest = (CardRequest) o;
         buildExpirationDate(cardRequest.getExpirationDate());
+        validateCreditCardNumber(cardRequest.getNumber());
         log.info("Card Name is " + cardRequest.getName());
     }
 
@@ -53,5 +54,30 @@ public class CardRequestValidator implements Validator {
         LocalDate initial = LocalDate.of(year, month,1);
         LocalDate end = initial.withDayOfMonth(initial.lengthOfMonth());
         return Integer.valueOf(end.toString().substring(8,10));
+    }
+
+    private void validateCreditCardNumber(String str) {
+        int[] ints = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ints[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = ints.length - 2; i >= 0; i = i - 2) {
+            int j = ints[i];
+            j = j * 2;
+            if (j > 9) {
+                j = j % 10 + 1;
+            }
+            ints[i] = j;
+        }
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            sum += ints[i];
+        }
+        if (sum % 10 == 0) {
+            log.info(str + " is a valid credit card number");
+        } else {
+            log.warn(str + " is an invalid credit card number");
+            throw new CardInvalidDataHttpException(str +  " is an invalid credit card number");
+        }
     }
 }
