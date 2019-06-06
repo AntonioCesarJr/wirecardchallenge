40525b7e81f7745a71633c3acbcc92882ee81c0d
AntonioCesarJr <jrcesar4@gmail.com>
2019-06-06 18:21:20 -0300

    Card validator validating expiration date - alway set day as last day of the month

--- wirecardchallenge-rest/src/main/java/com/wirecardchallenge/rest/controller/card/validator/CardRequestValidator.java
+++ wirecardchallenge-rest/src/main/java/com/wirecardchallenge/rest/controller/card/validator/CardRequestValidator.java
@@ -1,7 +1,7 @@
 package com.wirecardchallenge.rest.controller.card.validator;
 
-import com.wirecardchallenge.core.exceptions.card.CardInvalidDataException;
 import com.wirecardchallenge.rest.controller.card.request.CardRequest;
+import com.wirecardchallenge.rest.controller.exception.card.CardInvalidDataHttpException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -10,6 +10,8 @@ import java.time.LocalDate;
 
 @Slf4j
 public class CardRequestValidator implements Validator {
+
+    private final static String DEFAULT_INIT_YEAR = "20";
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -20,24 +22,36 @@ public class CardRequestValidator implements Validator {
     public void validate(Object o, Errors errors) {
         log.info("Card Validator in action !!");
         CardRequest cardRequest = (CardRequest) o;
+        buildExpirationDate(cardRequest.getExpirationDate());
         log.info("Card Name is " + cardRequest.getName());
     }
 
-    private LocalDate buildExpirationDate(String strDate) throws CardInvalidDataException {
-        LocalDate localDate = null;
-        if (strDate.length() > 5) {
-            log.warn("Invalid Length !!");
-            throw new CardInvalidDataException("Invalid Expiration Date (MM/yy)!!");
+    private void buildExpirationDate(String strDate)
+        throws CardInvalidDataHttpException {
+        LocalDate localDate;
+        if (strDate.length() != 5) {
+            log.warn("Invalid Expiration Date length !!");
+            throw new CardInvalidDataHttpException("Invalid Expiration Date length !!");
         }
         try{
             String month = strDate.substring(0,2);
-            String year = strDate.substring(3,5);
-            localDate = LocalDate.of(Integer.valueOf("20"+year),
-                Integer.valueOf(month),1);
+            String year = DEFAULT_INIT_YEAR + strDate.substring(3,5);
+            localDate = LocalDate.of(Integer.valueOf(year),
+                Integer.valueOf(month), lastDayOfMonth(Integer.valueOf(month),
+                    Integer.valueOf(year)));
         }catch (Exception e){
-            log.warn("Invalid Date Format (MM/yy) !!");
-            throw new CardInvalidDataException("Invalid Expiration Date (MM/yy)!!");
+            log.warn("Invalid Expiration Date Format (MM/yy) !!");
+            throw new CardInvalidDataHttpException("Invalid Expiration Date Format (MM/yy) !!");
         }
-        return localDate;
+        if (localDate.isBefore(LocalDate.now())) {
+            log.warn("Invalid Expiration Date !! Past Date !!");
+            throw new CardInvalidDataHttpException("Invalid Expiration Date !! Past Date !!");
+        }
+    }
+
+    private Integer lastDayOfMonth(Integer month, Integer year){
+        LocalDate initial = LocalDate.of(year, month,1);
+        LocalDate end = initial.withDayOfMonth(initial.lengthOfMonth());
+        return Integer.valueOf(end.toString().substring(8,10));
     }
 }
