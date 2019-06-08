74090817c9c1995f749dda00dd53d2218945e5d1
AntonioCesarJr <jrcesar4@gmail.com>
2019-06-08 12:31:10 -0300

    One more test

--- wirecardchallenge-rest/src/test/java/com/wirecardchallenge/rest/controller/client/ClientControllerTest.java
+++ wirecardchallenge-rest/src/test/java/com/wirecardchallenge/rest/controller/client/ClientControllerTest.java
@@ -1,6 +1,7 @@
 package com.wirecardchallenge.rest.controller.client;
 
 import com.wirecardchallenge.core.dto.ClientDto;
+import com.wirecardchallenge.core.exceptions.client.ClientNotFoundException;
 import com.wirecardchallenge.core.service.ClientService;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,8 +10,6 @@ import org.junit.runners.JUnit4;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.Any;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -32,17 +31,17 @@ import static org.mockito.Mockito.when;
 public class ClientControllerTest {
 
     private static final Long CLIENT_ID_1 = 1L;
-    private static final UUID CLIENT_PUBLICID_1 = UUID.randomUUID();
+    private static final UUID CLIENT_PUBLIC_ID_1 = UUID.randomUUID();
     private static final LocalDateTime CREATED_AT_1 = LocalDateTime.now();
     private static final LocalDateTime UPDATED_AT_1 = LocalDateTime.now().plusMinutes(10);
 
     private static final Long CLIENT_ID_2 = 2L;
-    private static final UUID CLIENT_PUBLICID_2 = UUID.randomUUID();
+    private static final UUID CLIENT_PUBLIC_ID_2 = UUID.randomUUID();
     private static final LocalDateTime CREATED_AT_2 = LocalDateTime.now();
     private static final LocalDateTime UPDATED_AT_2 = LocalDateTime.now().plusMinutes(15);
 
     private static final Long CLIENT_ID_3 = 3L;
-    private static final UUID CLIENT_PUBLICID_3 = UUID.randomUUID();
+    private static final UUID CLIENT_PUBLIC_ID_3 = UUID.randomUUID();
     private static final LocalDateTime CREATED_AT_3 = LocalDateTime.now();
     private static final LocalDateTime UPDATED_AT_3 = LocalDateTime.now().plusMinutes(20);
 
@@ -60,33 +59,61 @@ public class ClientControllerTest {
 
     @Test
     public void findAll() {
+
         List<ClientDto> clientDtos = buildClientDtoList();
         Page<ClientDto> clientDtoPage = new PageImpl<>(clientDtos);
+
         when(clientService.findAll(any(Pageable.class)))
             .thenReturn(clientDtoPage);
+
         ResponseEntity<Page<ClientDto>> clientPageResponse =
             clientController.findAll(PageRequest.of(1, 1));
+
         assertNotNull(clientPageResponse);
+        assertEquals(HttpStatus.OK, clientPageResponse.getStatusCode());
+        assertEquals(3,clientPageResponse.getBody().getTotalElements());
     }
 
     @Test
-    public void findByPublicId() {
+    public void findByPUBLIC_ID() throws ClientNotFoundException {
+
+        when(clientService.findByPublicId(CLIENT_PUBLIC_ID_1))
+            .thenReturn(ClientDto.builder()
+                .id(CLIENT_ID_1)
+                .publicId(CLIENT_PUBLIC_ID_1)
+                .createdAt(CREATED_AT_1)
+                .updatedAt(UPDATED_AT_1)
+                .build());
+
+        ResponseEntity<ClientDto> clientDtoResponse =
+            clientController.findByPublicId(CLIENT_PUBLIC_ID_1);
+
+        assertNotNull(clientDtoResponse);
+        assertEquals(HttpStatus.OK, clientDtoResponse.getStatusCode());
+        assertEquals(CLIENT_ID_1, clientDtoResponse.getBody().getId());
+        assertEquals(CLIENT_PUBLIC_ID_1, clientDtoResponse.getBody().getPublicId());
+        assertEquals(CREATED_AT_1, clientDtoResponse.getBody().getCreatedAt());
+        assertEquals(UPDATED_AT_1, clientDtoResponse.getBody().getUpdatedAt());
     }
 
     @Test
     public void add() {
+
         when(clientService.create()).thenReturn(ClientDto.builder()
             .id(CLIENT_ID_1)
-            .publicId(CLIENT_PUBLICID_1)
+            .publicId(CLIENT_PUBLIC_ID_1)
             .createdAt(CREATED_AT_1)
             .updatedAt(UPDATED_AT_1)
             .build());
-        ResponseEntity<ClientDto> clientDtoResponseEntity = clientController.add();
-        assertEquals(HttpStatus.OK, clientDtoResponseEntity.getStatusCode());
-        assertEquals(CLIENT_ID_1, clientDtoResponseEntity.getBody().getId());
-        assertEquals(CLIENT_PUBLICID_1, clientDtoResponseEntity.getBody().getPublicId());
-        assertEquals(CREATED_AT_1, clientDtoResponseEntity.getBody().getCreatedAt());
-        assertEquals(UPDATED_AT_1, clientDtoResponseEntity.getBody().getUpdatedAt());
+
+        ResponseEntity<ClientDto> clientDtoResponse = clientController.add();
+
+        assertNotNull(clientDtoResponse);
+        assertEquals(HttpStatus.OK, clientDtoResponse.getStatusCode());
+        assertEquals(CLIENT_ID_1, clientDtoResponse.getBody().getId());
+        assertEquals(CLIENT_PUBLIC_ID_1, clientDtoResponse.getBody().getPublicId());
+        assertEquals(CREATED_AT_1, clientDtoResponse.getBody().getCreatedAt());
+        assertEquals(UPDATED_AT_1, clientDtoResponse.getBody().getUpdatedAt());
     }
 
     @Test
@@ -94,25 +121,30 @@ public class ClientControllerTest {
     }
 
     private List<ClientDto> buildClientDtoList(){
+
         List<ClientDto> clientDtoList = new ArrayList<>();
+
         clientDtoList.add(ClientDto.builder()
             .id(CLIENT_ID_1)
-            .publicId(CLIENT_PUBLICID_1)
+            .publicId(CLIENT_PUBLIC_ID_1)
             .createdAt(CREATED_AT_1)
             .updatedAt(UPDATED_AT_1)
             .build());
+
         clientDtoList.add(ClientDto.builder()
             .id(CLIENT_ID_2)
-            .publicId(CLIENT_PUBLICID_2)
+            .publicId(CLIENT_PUBLIC_ID_2)
             .createdAt(CREATED_AT_2)
             .updatedAt(UPDATED_AT_2)
             .build());
+
         clientDtoList.add(ClientDto.builder()
             .id(CLIENT_ID_3)
-            .publicId(CLIENT_PUBLICID_3)
+            .publicId(CLIENT_PUBLIC_ID_3)
             .createdAt(CREATED_AT_3)
             .updatedAt(UPDATED_AT_3)
             .build());
+
         return clientDtoList;
     }
 }
